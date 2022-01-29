69297dfb459587d2e8c6981322dfd2a3639d938e
Iryna Halenok <iryna.halenok@gmail.com>
2022-01-29 08:55:48 +0200

    Add file uploading page

---
+++ src/components/FileUploader.tsx
@@ -0,0 +1,54 @@
+import {useState} from 'react';
+import { Grid, Typography, Input, Button } from "@mui/material";
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+
+interface FileUploaderProps {
+    ext: string;
+    onFileChange: (file: any) => void;
+  }
+
+const FileUploader = (props: FileUploaderProps) => {
+    const [selectedFile, setSelectedFile] = useState<any>();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const onFileChange = (event: any) => {
+        const selectedFile = event.target.files[0]
+        setSelectedFile(selectedFile);
+		setIsFilePicked(true);
+        props.onFileChange(selectedFile)
+    };
+
+    return (<>
+        <Grid>
+            <Typography variant="body1">
+                {`Add a .${props.ext} file`}
+            </Typography>
+        </Grid>
+        <Grid>
+            <label htmlFor={props.ext + "-file"}>
+                <Input 
+                    type="file" 
+                    inputProps={{ accept: `.${props.ext}` }} 
+                    style={{ display: 'none' }}
+                    id={props.ext + "-file"}
+                    onChange={onFileChange}
+                />
+                <Button
+                    variant="contained"
+                    component="span"
+                    startIcon={<UploadFileIcon />}>
+                    Choose File
+                </Button>
+            </label>
+        </Grid>
+        {
+            isFilePicked && (
+                <Grid>
+                    <p>Loaded file: {selectedFile && selectedFile.name}</p>
+                </Grid> 
+            )
+        }
+    </>)
+}
+
+export default FileUploader;
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,5 +1,50 @@
+import {useState} from 'react';
+import { Grid, Paper, Typography } from "@mui/material";
+import FileUploader from './FileUploader';
+
 const Upload = () => {
-    return <p>Upload</p>;
+    const [selectedBpmnFile, setSelectedBpmnFile] = useState<any>();
+    const [selectedJsonFile, setSelectedJsonFile] = useState<any>();
+
+    const onBpmnFileChange = (file: any) => {
+        setSelectedBpmnFile(file);
+        console.log(file)
+    };
+
+    const onJsonFileChange = (file: any) => {
+        setSelectedJsonFile(file);
+    };
+
+    return (
+        <Grid container alignItems="center" justifyContent="center">
+            <Grid item xs={12}>
+                <Typography variant="h6">
+                    Prosimos
+                </Typography>
+            </Grid>
+            <Grid item xs={9}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6} >
+                        <Paper elevation={5} sx={{ p: 3}}>
+                            <FileUploader
+                                ext="bpmn"
+                                onFileChange={onBpmnFileChange}
+                            />
+                        </Paper>
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <Paper elevation={5} sx={{ p: 3}}>
+                            <FileUploader
+                            ext="json"
+                            onFileChange={onJsonFileChange} 
+                            />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
 }
 
 export default Upload;
