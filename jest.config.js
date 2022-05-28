ae606a358d925003872dd7374dbcb40ac445c97d
Iryna Halenok <iryna.halenok@gmail.com>
2022-05-28 14:18:37 +0300

    #49 - Setup jest for running tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    "<rootDir>/src/setupTests.ts"
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(bpmn-js|diagram-js|@hookform/*|uuid))"]
+};
