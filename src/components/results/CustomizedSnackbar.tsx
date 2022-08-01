53ec0cf5e6446a39d395889b19175937df98d36e
Iryna Halenok <iryna.halenok@gmail.com>
2022-08-01 16:28:58 +0200

    Remove autoHide from all snackbars

--- src/components/results/CustomizedSnackbar.tsx
+++ src/components/results/CustomizedSnackbar.tsx
@@ -45,7 +45,7 @@ const CustomizedSnackbar = (props: CustomizedSnackbarProps) => {
     };
 
     return (
-        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Snackbar open={open} onClose={handleClose}>
             <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
                 {alertMessage}
             </Alert>
