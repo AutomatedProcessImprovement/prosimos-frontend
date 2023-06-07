d244cb49ab53a37348c8c48bc71dcf7c2618929c
Simple Squirrel <simplesquirrellearn@gmail.com>
2023-06-07 16:10:39 +0300

    Refactored WeekdayFilterCheckbox render function

--- src/components/calendars/WeekdayFilterCheckbox.tsx
+++ src/components/calendars/WeekdayFilterCheckbox.tsx
@@ -13,6 +13,15 @@ const WeekdayFilterCheckbox = (props: WeekdayFilterProps) => {
     return day.charAt(0).toUpperCase() + day.slice(1).toLowerCase();
   };
 
+  const renderSelectedValue = (selected:any) => {
+    return Array.isArray(selected) ? 
+      selected
+        .sort((a, b) => DAYS_OF_WEEK.indexOf(a) - DAYS_OF_WEEK.indexOf(b))
+        .map((day) => formatDay(day))
+        .join(', ') 
+      : selected
+  }
+
   return (
     <FormControl variant="standard" fullWidth>
       <InputLabel>{props.label || "Select at least one weekday"}</InputLabel>
@@ -20,14 +29,7 @@ const WeekdayFilterCheckbox = (props: WeekdayFilterProps) => {
         multiple
         value={props.value}
         onChange={props.onChange}
-        renderValue={(selected) => (
-          Array.isArray(selected) ? 
-            selected
-              .sort((a, b) => DAYS_OF_WEEK.indexOf(a) - DAYS_OF_WEEK.indexOf(b))
-              .map((day) => formatDay(day))
-              .join(', ') 
-            : selected
-        )}
+        renderValue={renderSelectedValue}
       >
         {DAYS_OF_WEEK.map((day) => (
           <MenuItem key={day} value={day}>
